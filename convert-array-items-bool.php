88c59f962231de0f5101d6a45f517b54db219cb4
nabtron <developer@nabtron.com>
2019-04-05 07:53:34 +0100

    Update convert-array-items-bool.php

--- convert-array-items-bool.php
+++ convert-array-items-bool.php
@@ -1,7 +1,8 @@
 <?php
 
 /*
-Imagine that you're receiving an array of 'group' inside $_POST and they have true false or 0 1 values, however http converts them to string when they are sent over to your server. 
+Imagine that you're receiving an array of 'group' inside $_POST and they have true false or 0 1 values, 
+however http converts them to string when they are sent over to your server. 
 This code will convert that array values back to php bool
 */
 
@@ -10,3 +11,4 @@ $group = $_POST['group'];
 foreach ( $group as &$group_el ) {
     $group_el = (bool)$group_el;
 }
+
